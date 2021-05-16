b9c0f88ffbec1cd7e9e164f4a507d5f4c9f7c74e
hepeng <2410465519@qq.com>
2021-05-17 00:43:59 +0800

    test

---
+++ src/test/java/mytest/MybatisTest.java
@@ -0,0 +1,29 @@
+package mytest;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+
+public class MybatisTest {
+
+
+    public static void main(String[] args) throws Exception {
+
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsReader("mytest/mybatis-config.xml"));
+
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+
+
+        Mapper mapper = sqlSession.getMapper(Mapper.class);
+        User user = mapper.getUser(1);
+        System.out.println(user);
+    }
+
+}
